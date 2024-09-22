3d512439cb99a3ae6e3b4ebbb954d7a24a3d5990
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2024-09-22 13:53:13 +0530

    Add files via upload
    IN Navbar darkmode /Lightmode feature

--- App.js
+++ App.js
@@ -3,20 +3,39 @@ import './App.css';
 import Navbar from './Componenets/Navbar';
 import Text from './Componenets/Text';
 import About from './Componenets/About';
-
+import React ,{ useState } from 'react';
 
 function App() {
+  const [mode, setmode] = useState('light')
+
+  const tooglemode=()=>{
+if(mode==='light'){
+  setmode('dark')
+  // document.body.style.backgroundColor="black";
+ 
+  }
+
+else
+{
+  setmode('light')
+  // document.body.style.backgroundColor="white";
+  
+}
+}
+      
+
   return (
     <>
    
-<Navbar Home="MyHome" Link="About" Dropdown="Services"Disabled="ContactUS"></Navbar>
+<Navbar Home="MyHome" Link="About" Dropdown="Services"Disabled="ContactUS"
+mode={mode} tooglemode={tooglemode} ></Navbar>
 <div>
   <div class="container my-4">
   <Text Haeding="Enter Text to Analyze Below"></Text>
   </div>
  </div>
 
- <About></About>
+ {/* <About></About> */}
   </>
 
   );
