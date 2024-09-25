c2c32067fab5ee209074bfb02f566d382e62818b
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2024-09-25 13:55:54 +0530

    RE uploaded

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Componenets/Navbar';
 import Text from './Componenets/Text';
 import About from './Componenets/About';
+import Number_from_text from './Componenets/Number_from_text';
 import React ,{ useState } from 'react';
 
 function App() {
@@ -11,31 +12,30 @@ function App() {
   const tooglemode=()=>{
 if(mode==='light'){
   setmode('dark')
-  // document.body.style.backgroundColor="black";
+   document.body.style.backgroundColor="#04045e";
  
   }
 
 else
 {
   setmode('light')
-  // document.body.style.backgroundColor="white";
+  document.body.style.backgroundColor="white";
   
 }
 }
-      
-
-  return (
+return (
     <>
    
 <Navbar Home="MyHome" Link="About" Dropdown="Services"Disabled="ContactUS"
 mode={mode} tooglemode={tooglemode} ></Navbar>
 <div>
   <div class="container my-4">
-  <Text Haeding="Enter Text to Analyze Below"></Text>
+  <Text Haeding="Enter Text to Analyze Below" mode={mode}></Text>
   </div>
  </div>
 
  {/* <About></About> */}
+ {/* <Number_from_text></Number_from_text> */}
   </>
 
   );
